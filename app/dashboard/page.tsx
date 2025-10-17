3ad5b6b882bfeca43f314cc13ee5eae8630f6f0b
amaniCoding <amanuelfrm@gmail.com>
2025-10-17 16:13:46 +0300

    refactor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,5 +2,5 @@ import prisma from "../libs/prisma";
 
 export default async function Page() {
   const users = await prisma.user.findMany();
-  return <p>{users[0].firstName}</p>;
+  return <p>{users.length > 0 ? users[0].firstName : null}</p>;
 }
