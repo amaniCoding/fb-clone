15fe3e9d40695236c5af851184e20da21698e329
amaniCoding <amanuelfrm@gmail.com>
2025-11-22 17:18:40 +0300

    thisthis

--- app/seed/feed/all/route.ts
+++ app/seed/feed/all/route.ts
@@ -1,20 +1,8 @@
-import {
-  _seedGroupPost,
-  _seedPagePost,
-  _seedPageSharePost,
-  _seedToGroupSharePost,
-  _seedUserPost,
-  _seedUserSharePost,
-} from "./lib";
+import { _seedAll } from "./lib";
 
 export async function GET() {
   try {
-    await _seedUserPost();
-    await _seedPagePost();
-    await _seedGroupPost();
-    await _seedUserSharePost();
-    await _seedPageSharePost();
-    await _seedToGroupSharePost();
+    await _seedAll();
     //await seedUser();
     return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
