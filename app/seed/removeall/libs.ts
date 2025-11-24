4333846e96d1c9426a315e2146a6e8941773e85c
amaniCoding <amanuelfrm@gmail.com>
2025-11-24 12:27:58 +0300

    thisthis

---
+++ app/seed/removeall/libs.ts
@@ -0,0 +1,14 @@
+import prisma from "@/app/libs/prisma";
+import { users } from "../dummy";
+import { Gender } from "@/app/generated/prisma";
+import bcrypt from "bcryptjs";
+
+export async function _remove() {
+  const r1 = prisma.oUserPost.deleteMany({});
+  const r2 = prisma.userSharePost.deleteMany({});
+  const r3 = prisma.oPagePost.deleteMany({});
+  const r4 = prisma.pageSharePost.deleteMany({});
+  const r5 = prisma.oGroupPost.deleteMany({});
+  const r6 = prisma.toGroupSharePost.deleteMany({});
+  return await Promise.all([r1, r2, r3, r4, r5, r6]);
+}
