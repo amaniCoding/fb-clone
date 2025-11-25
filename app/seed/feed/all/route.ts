b473a3edab6a4d9fda0de1488d8e2001a34e7aab
amaniCoding <amanuelfrm@gmail.com>
2025-11-25 10:31:05 +0300

    new

--- app/seed/feed/all/route.ts
+++ app/seed/feed/all/route.ts
@@ -3,7 +3,7 @@ import { _make_all_posts_dynamic, _update_all_posts } from "./lib";
 export async function GET() {
   try {
     await Promise.all([_update_all_posts(), _make_all_posts_dynamic()]);
-    return Response.json({ message: "Database seeded successfully" });
+    return Response.json({ message: " Database seeded successfully" });
   } catch (error) {
     console.log(error);
     return Response.json({ error }, { status: 500 });
