ccb2fd7ed2eb410002f14451ae51f2b464c12fd9
amaniCoding <amanuelfrm@gmail.com>
2025-12-09 15:40:51 +0300

    Changed UI

--- app/seed/owner/admin/libs.ts
+++ app/seed/owner/admin/libs.ts
@@ -1,22 +1,16 @@
 import prisma from "@/app/libs/prisma";
 
-export function _seeder() {
-  const udpate = prisma.user.update({
+export async function _seeder() {
+  await prisma.user.update({
     where: {
       id: "27997fea-b319-45bf-854a-bf4b5222f70f",
     },
     data: {
       Profile: {
         update: {
-          where: {
-            userId: "27997fea-b319-45bf-854a-bf4b5222f70f",
-          },
-          data: {
-            profilePicture: "/ad/aman.jpg",
-          },
+          profilePicture: "/ad/aman.jpg",
         },
       },
     },
   });
-  return udpate;
 }
